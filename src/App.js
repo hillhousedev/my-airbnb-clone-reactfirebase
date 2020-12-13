1d3397de8dd1f058a71330cbdcfded22afe35977
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-14 00:18:07 +0100

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Home from "./Home";
+
 
 function App() {
   return (
     <div className="app">
       <h1>AirBnB Clone</h1>
+
+      <Home />
+
     </div>
   );
 }
