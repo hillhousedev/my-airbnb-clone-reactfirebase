0d2a39bc6dbf23e9ee2aa7d5d077782fdbac6b1f
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-14 01:08:28 +0100

    Adding right elements of the header component

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,10 @@
-import React from 'react'
-import "./Header.css"
+import React from 'react';
+import "./Header.css";
+import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar } from "@material-ui/core";
+
 
 function Header() {
     return (
@@ -9,6 +14,19 @@ function Header() {
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1280px-Airbnb_Logo_B%C3%A9lo.svg.png"
                  alt=""
             />
+
+            <div className="header__center">
+                <input type="text" />
+                <SearchIcon />
+            </div>
+
+            <div className="header__right">
+                <p>Become a Host</p>
+                <LanguageIcon />
+                <ExpandMoreIcon />
+                <Avatar />
+                
+            </div>
             
         </div>
     )
