d372b392a4c1a68a8a1c0ba1e31d2968b6933e13
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-14 11:18:46 +0100

    Added banner component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="app">
       
       <Header />
-     
+      <Home />
 
     </div>
   );
---
+++ src/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Banner.css";
+
+function Banner() {
+    return (
+        <div className="banner">
+            <div className="banner__info">
+                <h1>Get Out and stetch your imagination</h1>
+            </div>
+              
+        </div>
+    )
+}
+
+export default Banner
