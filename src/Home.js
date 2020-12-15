898ddd0d9e02d2c74ec430307061a35add93bb8f
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-15 17:55:59 +0100

    Styling card component

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,19 @@ function Home() {
             <Banner />
 
             <div className="home__section">
-                <Card />
-                <Card />
-                <Card />
+                <Card 
+                    src="https://media.cntraveler.com/photos/5f5105abad72d4ef60c3a87c/master/w_1600%2Cc_limit/remote-33638284-airbnb.jpg"
+                    description="Unique activities we can do together, led by a world of hosts."
+                    title="Big house"
+                />
+                <Card 
+                    src="https://media.cntraveler.com/photos/5f5105aa42341b4d2fb70a1f/master/w_1600%2Cc_limit/remote-21783133-airbnb.jpg"
+                    description="A beautiful experience"
+                    title="Wooden Castle"/>
+                <Card 
+                    src="https://media.cntraveler.com/photos/5f510b99ad72d4ef60c3a87e/master/w_1600%2Cc_limit/remote-191013-airbnb.jpeg"
+                    description="Wholesome scene"
+                    title="Comfort Homes"/>
             </div>
 
             <div className="home__section">
