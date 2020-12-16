1db25d184f5f41e239172c603f84dadf05c16380
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-16 16:18:09 +0100

    Finished footer, added router and starting searchpage component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import './App.css';
 import Home from "./Home";
 import Header from "./Header";
 import Footer from "./Footer";
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
 
 function App() {
   return (
     <div className="app">
       
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+
+        <Header />
+
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          
+        </Switch>
+
+       
+        <Footer />
+
+      </Router>
+      
 
     </div>
   );
---
+++ src/SearchPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchPage() {
+    return (
+        <div className="search_page">
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default SearchPage
