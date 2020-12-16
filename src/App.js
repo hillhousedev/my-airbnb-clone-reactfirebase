ada5bb9f17d09da64ec74600c85b3b46cc855973
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-16 15:02:07 +0100

    Styling footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Footer.css";
+
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>@ 2020 AirBnB Clone! No rights reserved -
+                Do as you like with it
+            </p>
+            <p>Privacy . Terms . Sitemap . Company Details</p>
+        </div>
+    )
+}
+
+export default Footer
