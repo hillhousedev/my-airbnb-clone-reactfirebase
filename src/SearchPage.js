a96a01a0506e45370193c809872e51ac0bb55159
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-17 14:01:33 +0100

    Styling searchpageresult

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from '@material-ui/core';
 import "./SearchPage.css";
+import SearchResult from "./SearchResult";
 function SearchPage() {
     return (
         <div className="searchPage">
@@ -24,7 +25,28 @@ function SearchPage() {
                 <Button variant="outlined">More filters</Button>
             </div>
 
-            <SearchResult />
+            <SearchResult 
+                img="https://a0.muscache.com/im/pictures/fbe849a4-841a-41b3-b770-419402a6316f.jpg?im_w=320"
+                location="Private room in center of London"
+                title="House of Comfort"
+                description=" A place to find comfort"
+                star={4.73}
+                price="$50 / night"
+                total="$120 total"
+
+            />
+
+            <SearchResult
+                img="https://a0.muscache.com/im/pictures/fbe849a4-841a-41b3-b770-419402a6316f.jpg?im_w=320"
+                location="Private room in center of London"
+                title="House of Comfort"
+                description=" A place to find comfort"
+                star={4.73}
+                price="$50 / night"
+                total="$120 total"
+
+            />
+            
         </div>
     )
 }
